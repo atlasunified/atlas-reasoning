d2617adb35fda4ec2565d0ab8f1f89ceb710cae5
Atlas Unified <113645143+atlasunified@users.noreply.github.com>
2023-06-25 09:25:54 -0400

    Add files via upload

--- atlas_reasoning.py
+++ atlas_reasoning.py
@@ -17,8 +17,10 @@ def generate_text_files():
     with open('topics_subtopics.jsonl', 'r') as f:
         lines = f.readlines()
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        tasks = []
+    tasks = []
+    task_set = set()  # Define the task_set as an empty set
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        futures = []
         for line in lines:
             topic_data = json.loads(line)
             topic = topic_data["topic"]
@@ -30,28 +32,53 @@ def generate_text_files():
                     "subtopic": subtopic,
                     "retries": MAX_RETRIES
                 }
-                future = executor.submit(make_request, task)
-                tasks.append(future)
+                tasks.append(task)
 
-        while tasks:
-            done, tasks = concurrent.futures.wait(tasks, return_when=concurrent.futures.FIRST_COMPLETED)
+        for task in tasks:
+            task_set.add(str(task))  # Add every task to the task_set
+            futures.append(executor.submit(make_request, task))
+
+        while futures:
+            done, futures = concurrent.futures.wait(futures, return_when=concurrent.futures.FIRST_COMPLETED)
 
             for future in done:
-                if future.exception():
-                    task = future.result()
+                task = future.result()
+                if "exception" in task:
                     if task["retries"] > 0:
                         task["retries"] -= 1
                         print(f"Error on topic '{task['topic']}' and subtopic '{task['subtopic']}', retrying. {task['retries']} retries left.")
-                        tasks.add(executor.submit(make_request, task))
+                        futures.add(executor.submit(make_request, task))
                     else:
                         print(f"Error on topic '{task['topic']}' and subtopic '{task['subtopic']}', no retries left.")
                 else:
-                    print(f"Completed topic '{future.result()['topic']}' and subtopic '{future.result()['subtopic']}'.")
+                    task_set.remove(str(task))  # If the task was successful, remove it from the task_set
+                    print(f"Completed topic '{task['topic']}' and subtopic '{task['subtopic']}'.")
+
+    # Check if the set is empty at the end of processing
+    if not task_set:
+        print("All tasks have been completed successfully.")
+    else:
+        print(f"Following tasks were not completed: {task_set}")
+
+    # Check the existence of the output files in the directory for each task.
+    for task in tasks:
+        topic = task["topic"]
+        subtopic = task["subtopic"]
+        if not os.path.isfile(f'output/{topic}_{subtopic}.txt'):
+            print(f"Output file for task ({topic}, {subtopic}) does not exist.")
+        else:
+            print(f"Output file for task ({topic}, {subtopic}) is successfully written.")
 
 def make_request(task):
     topic = task["topic"]
     subtopic = task["subtopic"]
     retries = task["retries"]
+
+    # Check if the file exists, if yes, return the task and skip the rest
+    output_file = f'output/{topic}_{subtopic}.txt'
+    if os.path.isfile(output_file):
+        print(f"File '{output_file}' already exists, skipping request.")
+        return task
 
     print(f"\nInitiating request for topic '{topic}' and subtopic '{subtopic}' with {retries} retries left.")
 
@@ -66,17 +93,32 @@ def make_request(task):
                 "content": f"Generate 10 complex reasoning tasks on the topic of '{topic}' and sub-topic '{subtopic}'. Each task should include a prompt (a question or problem), a step-by-step reasoning (a logical explanation of how to arrive at the solution), and a solution (the final answer to the problem). The output format should be a list like this: ['prompt', 'step_by_step_reasoning', 'solution']. Do not deviate from the JSON format of {json.dumps({'id': 'string', 'task': [{'prompt': 'string', 'reasoning': 'string', 'solution': 'string'}]})}. Keep the tasks succinct, comprehensive, and avoid any unnecessary language. Keep all responses in JSON format and all on one line. Have these reasoning tasks on the PhD level and have high complexity factors."
             }
         ]
+
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages,
             max_tokens=3000  # Adjust this as needed
         )
-        # Prepare the response
-        response = f"{completion.choices[0].message['content']}"
+
+        # Try to parse the response as JSON
+        try:
+            response = json.loads(completion.choices[0].message['content'])
+        except json.JSONDecodeError:
+            # If it fails, handle the failure. For example, you might just write the
+            # raw output or manually convert the result into the desired format.
+            # The specific action depends on the specific requirements of your project.
+            response = {
+                "id": "fallback_id",
+                "task": [{
+                    "prompt": "Fallback prompt",
+                    "reasoning": "Fallback reasoning",
+                    "solution": "Fallback solution"
+                }]
+            }
 
         # Output the response to a text file named after the topic and subtopic
         with open(f'output/{topic}_{subtopic}.txt', 'w', encoding='utf-8') as f:
-            f.write(response)
+            f.write(json.dumps(response))
 
         print(f"Successful request for topic '{topic}' and subtopic '{subtopic}'. Writing to file.")
     except Exception as e:
@@ -89,8 +131,10 @@ task_id_counter = 0  # Global counter for task IDs
 
 def cleanse_text(line):
     global task_id_counter
+    # Remove newlines
+    clean_line = line.replace('\n', ' ')
     # Removing leading number and period
-    clean_line = re.sub(r'^\d+\.\s*', '', line)
+    clean_line = re.sub(r'^\d+\.\s*', '', clean_line)
     # Using regex to find and replace 'name' with modified 'name'
     match_name = re.search(r"'name': '([^']+)'", clean_line)
     if match_name:
@@ -106,7 +150,6 @@ def cleanse_text(line):
         task_id_counter += 1
     return clean_line
 
-import csv
 
 def text_to_csv(dir_path, output_file):
     files_list = os.listdir(dir_path)
@@ -114,39 +157,37 @@ def text_to_csv(dir_path, output_file):
 
     with open(output_file, 'w', encoding='utf-8', newline='') as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)  # Create a csv.writer
-        writer.writerow(['Prompt', 'Step-by-step reasoning', 'Solution'])  # Write header
+        writer.writerow(['id', 'Prompt', 'Step-by-step reasoning', 'Solution'])  # Write header
 
         for text_file in text_files_list:
             with open(os.path.join(dir_path, text_file), 'r', encoding='utf-8') as f:
-                content = f.read()
-
-                # join multiple lines into a single line and split by solutions
-                entries = content.replace('\n', ' ').split('Solution:')
-                for entry in entries:
-                    if entry.strip():  # ignore empty lines or lines only consisting of whitespace
-                        entry = entry.replace('=', '')  # remove any '=' sign
-                        _, _, after_prompt = entry.partition('Prompt:')
-                        prompt, _, after_reasoning = after_prompt.partition('Step-by-step reasoning:')
-                        reasoning = after_reasoning.strip()
-                        # As solutions were used as separator, they will be in the next entry in entries list
+                for line in f:
+                    line = cleanse_text(line).strip()
+                    # Ensure line isn't empty
+                    if line:
+                        # Load the JSON object from the line
                         try:
-                            solution = entries[entries.index(entry) + 1].split('Prompt:')[0] if (entries.index(entry) + 1) < len(entries) else ''
-                        except ValueError:
-                            print(f"Could not find entry in entries: {entry}")
-                            solution = ''  # or whatever default value makes sense in your case
-                        # strip everything past the period on the solution
-                        solution = solution.split('.')[0] 
+                            task = json.loads(line)
+                            id = task["id"]
+                            prompt = task["task"][0]["prompt"]
+                            reasoning = task["task"][0]["reasoning"]
+                            solution = task["task"][0]["solution"]
 
-                        # Check if all parts are non-empty
-                        if all([prompt.strip(), reasoning.strip(), solution.strip()]):  
+                            print(f"id: {id}")
+                            print(f"Prompt: {prompt}")
+                            print(f"Reasoning: {reasoning}")
+                            print(f"Solution: {solution}")
+
                             # Write to CSV directly, avoiding triple quotes
-                            writer.writerow([prompt.strip(), reasoning.strip(), solution.strip()])  # Use the writer to write the row
-
-            os.remove(os.path.join(dir_path, text_file))  # delete the txt file after converting
-
+                            writer.writerow([id, prompt, reasoning, solution])  # Use the writer to write the row
+                        except json.JSONDecodeError:
+                            print(f"Couldn't parse JSON from line: {line}")
+            # os.remove(os.path.join(dir_path, text_file))  # delete the txt file after converting
 
 
 def main():
+    if not os.path.exists('output'):
+        os.makedirs('output')
     generate_text_files()
     dir_path = 'output/'
     output_file = 'output.csv'
